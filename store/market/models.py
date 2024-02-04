4e270583957fbffd76ee473ab74b25432a783d5d
Murfy99 <112482232+Murfy99@users.noreply.github.com>
2024-02-04 11:16:45 +0330

    uuid

--- store/market/models.py
+++ store/market/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class Product(models.Model):
@@ -6,3 +7,25 @@ class Product(models.Model):
     price = models.PositiveIntegerField()
     count = models.PositiveIntegerField()
     description = models.TextField()
+    create_date = models.DateTimeField(auto_now_add = True)
+    last_edit_date = models.DateTimeField(auto_now = True)
+    uuid = models.UUIDField(default = uuid.uuid4, primary_key=True)
+    # discount = models.DecimalField(decimal_places=3)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+
+    
+# class Comment(models.Model):
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     text = models.TextField()
+
+# class Rank(models.Model):
+#     product = models.OneToOneField(Product, on_delete=models.CASCADE)
+#     rank = models.PositiveIntegerField()
+class Category(models.Model):
+    category_name = models.CharField(max_length = 25)
+    parent = models.ForeignKey('Category', on_delete=models.CASCADE,
+                               null = True, blank = True)
+    # null , blank baraye inke majbor nabashim hame parent dashte bashan
+# class Tag(models.Model):
+    # product = models.ManyToManyRel(Product, on_delete=models.CASCADE)
+#     tag_name = models.CharField(max_length = 25)
